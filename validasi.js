51f25ac42a803dd7e90df544d176e36c929eac9f
newagurl <45752478+serenewa@users.noreply.github.com>
2019-03-10 00:18:50 +0700

    Add files via upload

---
+++ validasi.js
@@ -0,0 +1,6 @@
+function huruf(evt){
+        var charCode = (evt.which) ? evt.which : event.keyCode
+        if ((charCode < 65 || charCode > 90)&&(charCode < 97 || charCode > 122)&&charCode>32)
+            return false;
+        return true;
+}
